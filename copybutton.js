92bb07dfa6724b2f7d7976596b25e845a89bc857
Xyzz <hckrxyzz@gmail.com>
2025-07-21 15:06:08 +0600

    Create copybutton.js
    Signed-off-by: Xyzz <hckrxyzz@gmail.com>

---
+++ copybutton.js
@@ -0,0 +1,24 @@
+  document.addEventListener("DOMContentLoaded", function () {
+            // Select all elements with class 'copyBtn'
+            document.querySelectorAll(".copyBtn").forEach((element) => {
+                // Find all input fields inside each element
+                element.querySelectorAll("input").forEach((input) => {
+                    // Create a copy button
+                    const copyButton = document.createElement("button");
+                    copyButton.innerText = "Copy";
+                    copyButton.style.marginLeft = "5px";
+
+                    // When button is clicked, copy input value
+                    copyButton.addEventListener("click", () => {
+                        navigator.clipboard.writeText(input.value).then(() => {
+                            alert("Copied to clipboard!");
+                        }).catch((err) => {
+                            console.error("Failed to copy: ", err);
+                        });
+                    });
+
+                    // Insert the button after the input field
+                    input.parentNode.insertBefore(copyButton, input.nextSibling);
+                });
+            });
+        });

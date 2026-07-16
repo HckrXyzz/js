d77ecb67ab4389308f25b1c4e9b11be0d773ff25
Xyzz <hckrxyzz@gmail.com>
2026-07-16 14:17:39 +0600

    Update commonheader.js
    Signed-off-by: Xyzz <hckrxyzz@gmail.com>

--- commonheader.js
+++ commonheader.js
@@ -1,4 +1,5 @@
+const commonPage = document.getElementById("commonPage");
 const commonHeader = document.createElement("header");
-commonHeader id="header" ;
-commonHeader.classList="fixed top-0 left-0 h-16 right-0 z-50 bg-slate-900 shadow-xl shadow-black backdrop-blur";
-document.getElementBy("commonPage").append(commonHeader);
+commonHeader.id="header" ;
+commonHeader.classList="commonHeader fixed top-0 left-0 h-16 right-0 z-50 bg-slate-900 shadow-xl shadow-black backdrop-blur";
+commonPage.append(commonHeader);

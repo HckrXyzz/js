e313d00230c74a815133fbbabbd2aafcf1371744
Xyzz <hckrxyzz@gmail.com>
2026-07-16 14:13:20 +0600

    Add common header to commonheader.js
    Signed-off-by: Xyzz <hckrxyzz@gmail.com>

---
+++ commonheader.js
@@ -0,0 +1,4 @@
+const commonHeader = document.createElement("header");
+commonHeader id="header" ;
+commonHeader.classList="fixed top-0 left-0 h-16 right-0 z-50 bg-slate-900 shadow-xl shadow-black backdrop-blur";
+document.getElementBy("commonPage").append(commonHeader);
